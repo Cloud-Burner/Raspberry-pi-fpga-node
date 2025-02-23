72fde868ac775086647238d554045d208ba49305
Maksprots <makslive@bk.ru>
2025-02-23 22:23:09 +0300

    feat: python set old vers

--- raspberry_pi_fpga_node/__main__.py
+++ raspberry_pi_fpga_node/__main__.py
@@ -14,7 +14,7 @@ from openocd import OpenOcd
 with OpenOcd() as oocd:
     oocd.halt()
     registers = oocd.read_registers(['pc', 'sp'])
-    oocd.execute("openocd -f /usr/share/openocd/scripts/interface/altera-usb-blaster.cfg    -f /usr/share/openocd/scripts/target/fpga.cfg -c "init; svf /home/adm/output.svf; shutdown"\”")
+    oocd.execu te("openocd -f /usr/share/openocd/scripts/interface/altera-usb-blaster.cfg    -f /usr/share/openocd/scripts/target/fpga.cfg -c "init; svf /home/adm/output.svf; shutdown"\”")
 
     print('Program counter: 0x%x' % registers['pc'])
     print('Stack pointer: 0x%x' % registers['sp'])
