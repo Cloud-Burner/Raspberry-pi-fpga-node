fee1678c965b537d49c2768ddb188db230b27dee
Maksprots <makslive@bk.ru>
2025-02-23 16:01:20 +0300

    feat: restruct, test oocd

---
+++ raspberry_pi_fpga_node/__main__.py
@@ -0,0 +1,22 @@
+#
+# from openocd import OpenOcd
+#
+# with OpenOcd() as oocd:
+#     oocd.halt()
+#     registers = oocd.read_registers(['pc', 'sp'])
+#
+#     print('Program counter: 0x%x' % registers['pc'])
+#     print('Stack pointer: 0x%x' % registers['sp'])
+#
+#     oocd.resume()
+
+from openocd import OpenOcd
+with OpenOcd() as oocd:
+    oocd.halt()
+    registers = oocd.read_registers(['pc', 'sp'])
+    oocd.execute("openocd -f /usr/share/openocd/scripts/interface/altera-usb-blaster.cfg    -f /usr/share/openocd/scripts/target/fpga.cfg -c "init; svf /home/adm/output.svf; shutdown"\”")
+
+    print('Program counter: 0x%x' % registers['pc'])
+    print('Stack pointer: 0x%x' % registers['sp'])
+
+    oocd.resume()
