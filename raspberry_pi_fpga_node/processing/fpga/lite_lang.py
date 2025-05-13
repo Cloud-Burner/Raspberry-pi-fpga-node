747fc15a3d016b270f31c4eabfa9dec22b1b2300
Maksprots <makslive@bk.ru>
2025-05-13 16:53:22 +0300

    feat: add avr

--- raspberry_pi_fpga_node/processing/fpga/lite_lang.py
+++ raspberry_pi_fpga_node/processing/fpga/lite_lang.py
@@ -12,7 +12,7 @@ from raspberry_pi_fpga_node.processing.fpga.command_proccessing_base import (
 
 class LiteLangExecutor(CommandProcessingBase):
     def __init__(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        super().__init__(*args, **kwargs)
+        super().__init__(kwargs.get("instruction"))
         self.map_commands = {
             "pin": self._set_pin_state,
             "write_frame": self._add_frame_amount,
