ff45f69ccc56222c08bddf28cb92a4076ca2b630
Maksprots <makslive@bk.ru>
2025-05-13 12:12:28 +0300

    fix: logs

--- raspberry_pi_fpga_node/processing/fpga/executor.py
+++ raspberry_pi_fpga_node/processing/fpga/executor.py
@@ -32,41 +32,40 @@ lock = threading.Lock()
 
 async def fpga_process(task: FpgaTask) -> None:
     """Make all task processes asynchronously in parallel thread."""
-    with lock:
-        instruction = await download(
-            bucket=settings.task_bucket, file=task.instruction_file
-        )
-        logger.info(instruction)
-        flash_file = await download(bucket=settings.task_bucket, file=task.flash_file)
+    instruction = await download(
+        bucket=settings.task_bucket, file=task.instruction_file
+    )
+    logger.info(instruction)
+    flash_file = await download(bucket=settings.task_bucket, file=task.flash_file)
 
-        name = (
-            str(task.user_id) + "-" + task.number + "-" + str(time()).replace(".", "-")
-        )
+    name = (
+        str(task.user_id) + "-" + task.number + "-" + str(time()).replace(".", "-")
+    )
 
-        with tempfile.NamedTemporaryFile(
-            delete=True, suffix=".svf", dir=Path(settings.dynamic_dir)
-        ) as temp_file:
-            temp_file.write(flash_file)
-            temp_file.flush()
-            flasher.flash_fpga(flash_file_path=temp_file.name)
-            video = camera.get_video(
-                command_processor=LiteLangExecutor(instruction=instruction),
-                position=settings.fpga_camera_position,
-            )
-            link = await upload_bytes(
-                bucket=settings.result_bucket,
-                file=video,
-                name=name + f".{settings.video_format}",
-            )
-            logger.info(f"Vido uploaded, download on {link}")
-            logger.info(f"Tempfile {temp_file.name} deleted")
-        await broker.publish(
-            message=ResultFpgaTask(
-                user_id=task.user_id, number=task.number, link=str(link)
-            ),
-            queue=result_queue,
+    with tempfile.NamedTemporaryFile(
+        delete=True, suffix=".svf", dir=Path(settings.dynamic_dir)
+    ) as temp_file:
+        temp_file.write(flash_file)
+        temp_file.flush()
+        flasher.flash_fpga(flash_file_path=temp_file.name)
+        video = camera.get_video(
+            command_processor=LiteLangExecutor(instruction=instruction),
+            position=settings.fpga_camera_position,
         )
-        logger.info(f"Result sent to user:{task.user_id}")
+        link = await upload_bytes(
+            bucket=settings.result_bucket,
+            file=video,
+            name=name + f".{settings.video_format}",
+        )
+        logger.info(f"Vido uploaded, download on {link}")
+        logger.info(f"Tempfile {temp_file.name} deleted")
+    await broker.publish(
+        message=ResultFpgaTask(
+            user_id=task.user_id, number=task.number, link=str(link)
+        ),
+        queue=result_queue,
+    )
+    logger.info(f"Result sent to user:{task.user_id}")
 
 
 async def async_arduino_nano_process(task: ArduinoTask) -> None:
