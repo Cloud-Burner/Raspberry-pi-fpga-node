9ce4e21198391ee4099fc2ced51b363ba169cf89
Maksprots <99216054+Maksprots@users.noreply.github.com>
2025-05-29 09:47:35 +0300

    Feature/avr pools (#11)
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * feat: add avr
    
    * fix: fix concurrent frame recieving
    
    * feat: add avr
    
    * fix: non usable

--- raspberry_pi_fpga_node/processing/fpga/video_write.py
+++ raspberry_pi_fpga_node/processing/fpga/video_write.py
@@ -1,13 +1,17 @@
 """This module contains the processor that writes video and execute lang."""
 
 import tempfile
+import time
 
 import cv2
+from loguru import logger
 
 from raspberry_pi_fpga_node.core.settings import settings
 from raspberry_pi_fpga_node.processing.fpga.command_proccessing_base import (
     CommandProcessingBase,
 )
+
+MAX_ERROR_COUNT = 200
 
 
 class VideoWriter:
@@ -16,23 +20,29 @@ class VideoWriter:
     def __init__(self) -> None:
         self.camera = cv2.VideoCapture(settings.camera_number)
         self.fourcc = cv2.VideoWriter_fourcc(*settings.fourcc_codec)
-        self.width = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH)) // 2
-        self.height = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT)) // 2
 
     def get_video(
         self, command_processor: CommandProcessingBase, position: int
     ) -> bytes:
+        logger.info("Video start")
         with tempfile.NamedTemporaryFile(suffix=".mp4") as tmp:
             out = cv2.VideoWriter(
                 tmp.name, self.fourcc, settings.fps, (self.width, self.height)
             )
-            while command_processor.execution_state:
+            errors_count = 0
+            while command_processor.execution_state and errors_count < MAX_ERROR_COUNT:
+                time.sleep(0.05)
                 ret, frame = self.camera.read()
-                command_processor.next()
                 if not ret:
-                    break
+                    errors_count += 1
+                    logger.warning(f"Didn't receive frame: {errors_count}")
+                    continue
+
+                command_processor.next()
                 frame_cut = (
-                    frame[:, : self.width] if not position else frame[:, self.width :]
+                    frame[: self.height, :] if not position else frame[self.height :, :]
                 )
                 out.write(frame_cut)
             out.release()
